b5b5ae5731756801f7e652bd5d23e5c480e88c11
Dorime <90113933+yaderxd@users.noreply.github.com>
2022-05-19 10:21:06 -0600

    Add files via upload

---
+++ PruebaProg.Domain/Entities/Dominios.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PruebaProg.Domain
+{
+    public class Dominios
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Apellido { get; set; }
+        public int Carnet { get; set; }
+        public int Phone { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+        public int Matematica { get; set; }
+        public int Contabilidad { get; set; }
+        public int Programacion { get; set; }
+        public int Estadistica { get; set; }
+    }
+}
